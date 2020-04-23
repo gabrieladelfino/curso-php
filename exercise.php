14e0544385684a811b0d6286b14b722fb27a7118
gabrieladelfino <gabriela.delfino@outlook.com.br>
2020-04-23 12:32:24 -0300

    Cria menu de navegação e página de exibição dos exercicios

---
+++ exercise.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/exercise.css">
+  <title>Curso PHP</title>
+</head>
+
+<body class="exercise">
+  <header class="header">
+    <h1>Curso PHP</h1>
+    <h2>Visualização dos exercicios</h2>
+  </header>
+<nav class="navigation">
+  <a href="#" class="green">Sem formatação</a>
+  <a href="index.php" class="red">Voltar</a>
+</nav>
+  <main class="main">
+    <div class="content">
+
+    </div>
+  </main>
+  <footer class="footer">
+    COD3R & ALUNOS ® <?= date('Y'); ?>
+  </footer>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
   <title>Curso PHP</title>
 </head>
@@ -13,7 +12,7 @@
 <body>
   <header class="header">
     <h1>Curso PHP</h1>
-    <h2>Indice dos Exercicios</h2>
+    <h2>Indice dos exercicios</h2>
   </header>
   <main class="main">
     <div class="content">
@@ -21,79 +20,79 @@
         <div class="module">
           <h3>Módulo 01 - Básico</h3>
           <ul>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
           </ul>
         </div>
         <div class="module">
           <h3>Módulo 02 - Básico</h3>
           <ul>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
           </ul>
         </div>
         <div class="module">
           <h3>Módulo 03 - Básico</h3>
           <ul>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
           </ul>
         </div>
         <div class="module">
           <h3>Módulo 04 - Básico</h3>
           <ul>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
           </ul>
         </div>
         <div class="module">
           <h3>Módulo 05 - Básico</h3>
           <ul>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
           </ul>
         </div>
         <div class="module">
           <h3>Módulo 06 - Básico</h3>
           <ul>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
           </ul>
         </div>
         <div class="module">
           <h3>Módulo 07 - Básico</h3>
           <ul>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
           </ul>
         </div>
         <div class="module">
           <h3>Módulo 8 - Básico</h3>
           <ul>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
           </ul>
         </div>
         <div class="module">
           <h3>Módulo 09 - Básico</h3>
           <ul>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
-            <li><a href="#">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
+            <li><a href="exercise.php">Exercicio A</a></li>
           </ul>
         </div>
       </nav>
     </div>
   </main>
-  <footer class="footer"> 
+  <footer class="footer">
     COD3R & ALUNOS ® <?= date('Y'); ?>
   </footer>
 </body>
